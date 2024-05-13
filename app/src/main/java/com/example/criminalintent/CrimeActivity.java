f9e8243e0c38dc0cda0b3926fbbae4a9efc3be05
Madelene <dmadeline2003@gmail.com>
2024-05-13 16:14:49 +0800

    inti

---
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,28 @@
+package com.example.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class CrimeActivity extends FragmentActivity {
+    public CrimeActivity() {
+        super();
+    }
+    protected void OnCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_crime);
+//
+//        FragmentManager fm = getSupportFragmentManager();
+////        Fragment fragment = fm.findFragmentById(R.id.fragmentContainerView);
+//        if (fragment == null) {
+//            fragment = new CrimeFragment();
+//            fm.beginTransaction()
+////                    .add(R.id.fragmentContainerView, fragment)
+//                    .commit();
+//        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -0,0 +1,36 @@
+//package com.example.criminalintent;
+//
+//import android.content.Context;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.UUID;
+//
+//public class CrimeLab {
+//    private static CrimeLab sCrimeLab;
+//
+//    private List<Crime> mCrimes;
+//    public static CrimeLab get(Context context) {
+////        mCrime = new ArrayList<>();
+////        for (int i = 0; i ,100; i++) {
+////            Crime crime = new Crime();
+////            crime.setTitle("Crime #" + i);
+////            crime.setSolved(i % 2 == 0);
+//////            mCrimes.add(crime);
+////        }
+////    }
+//    private CrimeLab(Context context) {
+//        mCrimes = new ArrayList<>();
+//    }
+//    public List<Crime> getCrimes() {
+//        return mCrimes;
+//    }
+//    public Crime getCrime(UUID id) {
+//        for (Crime crime : mCrimes) {
+//            if (crime.getId().equals(id)) {
+//                return crime;
+//            }
+//        }
+//        return null;
+//    }
+//}
